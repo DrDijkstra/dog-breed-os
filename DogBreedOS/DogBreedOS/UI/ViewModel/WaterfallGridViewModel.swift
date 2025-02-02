dc75edda9da4898ea7b8a25eb4a0c8d60f8d5534
DrDijkstra <deysanjay285@gmail.com>
2025-02-01 22:34:32 -0500

    code refactored

--- DogBreedOS/DogBreedOS/UI/ViewModel/WaterfallGridViewModel.swift
+++ DogBreedOS/DogBreedOS/UI/ViewModel/WaterfallGridViewModel.swift
@@ -10,23 +10,39 @@ import UIKit
 import Combine
 
 protocol CardImageProvider: AnyObject {
-   func updateCardImagesList(_ breedImages: [CardData])
+    func updateCardImagesList(_ breedImages: [CardData])
 }
 
-class WaterfallGridViewModel: ObservableObject , CardImageProvider{
-    func updateCardImagesList(_ breedImages: [CardData]) {
-        updateColumns(with: breedImages)
-    }
+class WaterfallGridViewModel: ObservableObject, CardImageProvider {
     
+    // MARK: - Published Properties
     @Published var columns: [[CardData]] = []
     
+    // MARK: - Private Properties
     private let numberOfColumns: Int
     private var cancellables = Set<AnyCancellable>()
     
+    private var columnSpacing: CGFloat = 48
+    private var horizontalPadding: CGFloat = 32
+    
+    var imageWidth: CGFloat {
+        let screenWidth = UIScreen.main.bounds.width
+        let totalSpacing = CGFloat(numberOfColumns - 1) * columnSpacing
+        let totalPadding = horizontalPadding * 2
+        return (screenWidth - totalSpacing - totalPadding) / CGFloat(numberOfColumns)
+    }
+    
+    // MARK: - Initialization
     init(numberOfColumns: Int) {
         self.numberOfColumns = numberOfColumns
     }
     
+    // MARK: - Public Methods
+    func updateCardImagesList(_ breedImages: [CardData]) {
+        updateColumns(with: breedImages)
+    }
+    
+    // MARK: - Private Methods
     private func updateColumns(with breedImages: [CardData]) {
         let newColumns = distributeImagesAcrossColumns(breedImages: breedImages)
         
@@ -37,7 +53,6 @@ class WaterfallGridViewModel: ObservableObject , CardImageProvider{
     
     private func distributeImagesAcrossColumns(breedImages: [CardData]) -> [[CardData]] {
         var columns: [[CardData]] = Array(repeating: [], count: numberOfColumns)
-        
         var columnHeights: [CGFloat] = Array(repeating: 0, count: numberOfColumns)
         
         for breedImage in breedImages {
@@ -58,10 +73,4 @@ class WaterfallGridViewModel: ObservableObject , CardImageProvider{
         return imageWidth * aspectRatio
     }
     
-    var imageWidth: CGFloat {
-        let screenWidth = UIScreen.main.bounds.width
-        let totalSpacing = CGFloat(numberOfColumns - 1) * 48 // Spacing between columns
-        let totalPadding: CGFloat = 32 // Horizontal padding (16 on each side)
-        return (screenWidth - totalSpacing - totalPadding) / CGFloat(numberOfColumns)
-    }
 }
